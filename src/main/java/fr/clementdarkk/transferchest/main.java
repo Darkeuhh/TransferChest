42e98b1158e37acc0cc1defac7deb73e8b0fa48d
Darkeuhh <darckdemforge@gmail.com>
2023-02-21 13:11:40 +0100

    Création

---
+++ src/main/java/fr/clementdarkk/transferchest/main.java
@@ -0,0 +1,12 @@
+package fr.clementdarkk.transferchest;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        System.out.println("Plugin enable");
+
+    }
+}
